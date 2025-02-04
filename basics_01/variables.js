2cfdcb977bf078be30453e54f2e1f4e51bfa785f
VaibhavK289 <116457154+VaibhavK289@users.noreply.github.com>
2025-02-04 07:13:48 +0000

    experimenting

---
+++ basics_01/variables.js
@@ -0,0 +1,16 @@
+const accountId = 12334
+let accountEmail = "vaibhav.1@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Jaipur"
+
+console.log(accountId)
+console.log((accountEmail))
+console.log(accountPassword)
+console.log(accountCity)
+
+//accountId = 22222
+//accountEmail = "vaibhav.vaibhav@rediffmail.com"
+//console.log(accountEmail)
+
+accountPassword = "222222"
+console.log(accountPassword)
